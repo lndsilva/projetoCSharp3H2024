0e6427ab8ba370e963ba0b44a2438245c7ec024c
lndsilva <lndsilva@hotmail.com>
2024-03-01 21:32:09 -0300

    janela de menu

--- ProjetoLojaABC/frmMenuPrincipal.Designer.cs
+++ ProjetoLojaABC/frmMenuPrincipal.Designer.cs
@@ -29,10 +29,22 @@ namespace ProjetoLojaABC
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuPrincipal));
+            this.SuspendLayout();
+            // 
+            // frmMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmMenuPrincipal";
+            this.ClientSize = new System.Drawing.Size(780, 557);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LojaABC - Menu principal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
