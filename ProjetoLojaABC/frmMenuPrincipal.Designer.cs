d2922164abcc705d1a847776d420f7a285576af6
lndsilva <lndsilva@hotmail.com>
2024-03-01 21:53:10 -0300

    janela de menu Botões novos

--- ProjetoLojaABC/frmMenuPrincipal.Designer.cs
+++ ProjetoLojaABC/frmMenuPrincipal.Designer.cs
@@ -30,74 +30,74 @@ namespace ProjetoLojaABC
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuPrincipal));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.btnFuncionarios = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnProdutos = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.btnFornecedores = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnClientes
             // 
-            this.button1.Location = new System.Drawing.Point(24, 28);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(211, 220);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnClientes.Location = new System.Drawing.Point(24, 28);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(211, 220);
+            this.btnClientes.TabIndex = 0;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnFuncionarios
             // 
-            this.button2.Location = new System.Drawing.Point(24, 267);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(211, 220);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnFuncionarios.Location = new System.Drawing.Point(24, 267);
+            this.btnFuncionarios.Name = "btnFuncionarios";
+            this.btnFuncionarios.Size = new System.Drawing.Size(211, 220);
+            this.btnFuncionarios.TabIndex = 1;
+            this.btnFuncionarios.Text = "Fucionários";
+            this.btnFuncionarios.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(285, 267);
+            this.button3.Location = new System.Drawing.Point(283, 267);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(211, 220);
             this.button3.TabIndex = 3;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnProdutos
             // 
-            this.button4.Location = new System.Drawing.Point(285, 28);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(211, 220);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnProdutos.Location = new System.Drawing.Point(283, 28);
+            this.btnProdutos.Name = "btnProdutos";
+            this.btnProdutos.Size = new System.Drawing.Size(211, 220);
+            this.btnProdutos.TabIndex = 2;
+            this.btnProdutos.Text = "Produtos";
+            this.btnProdutos.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(520, 267);
+            this.button5.Location = new System.Drawing.Point(543, 267);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(211, 220);
             this.button5.TabIndex = 5;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnFornecedores
             // 
-            this.button6.Location = new System.Drawing.Point(520, 28);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(211, 220);
-            this.button6.TabIndex = 4;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnFornecedores.Location = new System.Drawing.Point(543, 28);
+            this.btnFornecedores.Name = "btnFornecedores";
+            this.btnFornecedores.Size = new System.Drawing.Size(211, 220);
+            this.btnFornecedores.TabIndex = 4;
+            this.btnFornecedores.Text = "Fornecedores";
+            this.btnFornecedores.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(573, 495);
+            this.button7.Location = new System.Drawing.Point(543, 495);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(194, 52);
+            this.button7.Size = new System.Drawing.Size(215, 52);
             this.button7.TabIndex = 6;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
@@ -109,11 +109,11 @@ namespace ProjetoLojaABC
             this.ClientSize = new System.Drawing.Size(780, 557);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnFornecedores);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnProdutos);
+            this.Controls.Add(this.btnFuncionarios);
+            this.Controls.Add(this.btnClientes);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -126,12 +126,12 @@ namespace ProjetoLojaABC
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnFuncionarios;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnProdutos;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnFornecedores;
         private System.Windows.Forms.Button button7;
     }
 }
