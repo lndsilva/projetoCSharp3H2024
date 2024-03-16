90eb3ceb72c0234cbafcaca195add1249a4e8f85
lndsilva <lndsilva@hotmail.com>
2024-03-15 21:36:23 -0300

    janela de funcionarios n

--- ProjetoLojaABC/frmFuncionarios.Designer.cs
+++ ProjetoLojaABC/frmFuncionarios.Designer.cs
@@ -33,14 +33,18 @@ namespace ProjetoLojaABC
             this.btnVoltar = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnVoltar
             // 
-            this.btnVoltar.Location = new System.Drawing.Point(632, 10);
+            this.btnVoltar.Location = new System.Drawing.Point(656, 10);
             this.btnVoltar.Name = "btnVoltar";
-            this.btnVoltar.Size = new System.Drawing.Size(133, 42);
+            this.btnVoltar.Size = new System.Drawing.Size(120, 42);
             this.btnVoltar.TabIndex = 0;
             this.btnVoltar.Text = "Voltar";
             this.btnVoltar.UseVisualStyleBackColor = true;
@@ -48,6 +52,10 @@ namespace ProjetoLojaABC
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.btnVoltar);
             this.panel1.Location = new System.Drawing.Point(3, 486);
@@ -59,10 +67,46 @@ namespace ProjetoLojaABC
             // 
             this.button1.Location = new System.Drawing.Point(7, 10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 42);
+            this.button1.Size = new System.Drawing.Size(120, 42);
             this.button1.TabIndex = 1;
             this.button1.Text = "Voltar";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Voltar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(264, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Voltar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(393, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 42);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Voltar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(517, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 42);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Voltar";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // frmFuncionarios
             // 
@@ -86,5 +130,9 @@ namespace ProjetoLojaABC
         private System.Windows.Forms.Button btnVoltar;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
