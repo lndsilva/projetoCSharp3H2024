81201c41c39345e6789e598b46d23dea1e179338
lndsilva <lndsilva@hotmail.com>
2024-03-15 21:34:27 -0300

    janela de funcionarios

--- ProjetoLojaABC/frmFuncionarios.Designer.cs
+++ ProjetoLojaABC/frmFuncionarios.Designer.cs
@@ -31,30 +31,52 @@ namespace ProjetoLojaABC
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFuncionarios));
             this.btnVoltar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnVoltar
             // 
-            this.btnVoltar.Location = new System.Drawing.Point(634, 499);
+            this.btnVoltar.Location = new System.Drawing.Point(632, 10);
             this.btnVoltar.Name = "btnVoltar";
-            this.btnVoltar.Size = new System.Drawing.Size(125, 46);
+            this.btnVoltar.Size = new System.Drawing.Size(133, 42);
             this.btnVoltar.TabIndex = 0;
             this.btnVoltar.Text = "Voltar";
             this.btnVoltar.UseVisualStyleBackColor = true;
             this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnVoltar);
+            this.panel1.Location = new System.Drawing.Point(3, 486);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(779, 71);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Voltar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // frmFuncionarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(780, 557);
-            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmFuncionarios";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LojaABC - Funcionários";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -62,5 +84,7 @@ namespace ProjetoLojaABC
         #endregion
 
         private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
