fa0fdcf88a3ef876227bdb36b560e2d7a5d72596
lndsilva <lndsilva@hotmail.com>
2024-05-03 22:13:14 -0300

    banco de dados

---
+++ dbloja.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Maio-2024 às 03:12
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `dbloja`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbfuncionarios`
+--
+
+CREATE TABLE `tbfuncionarios` (
+  `codFunc` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `cpf` char(14) NOT NULL,
+  `telCel` char(10) DEFAULT NULL,
+  `endereco` varchar(100) DEFAULT NULL,
+  `numero` char(5) DEFAULT NULL,
+  `cep` char(9) DEFAULT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `estado` char(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `tbfuncionarios`
+--
+
+INSERT INTO `tbfuncionarios` (`codFunc`, `nome`, `email`, `cpf`, `telCel`, `endereco`, `numero`, `cep`, `bairro`, `cidade`, `estado`) VALUES
+(1, 'João da Silva', 'joao.silva@hotmail.com', '253.352.685-77', '99582-8547', 'Rua das Pedras', '55', '04752-888', 'Santo Amaro', 'São Paulo', 'SP');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tbfuncionarios`
+--
+ALTER TABLE `tbfuncionarios`
+  ADD PRIMARY KEY (`codFunc`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tbfuncionarios`
+--
+ALTER TABLE `tbfuncionarios`
+  MODIFY `codFunc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
